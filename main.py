d5638e499c97225766f22203ef75d0a3bca8bf19
Jerome Siljan <Jerome.Siljan@mavs.uta.edu>
2022-04-16 22:53:19 -0500

    able to pull discography

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-imporconcurrent.futures
 import threading
 import time
 import musicbrainzngs
@@ -29,38 +28,14 @@ def add_tracks(title_num, discography, input_artist, album_list):
     raw_tracks_list = musicbrainzngs.get_release_by_id(album_id, includes=["recordings"])
     tracks_list = (raw_tracks_list["release"]["medium-list"][0]["track-list"])
     for track in tracks_list:
-        #print("\t" + track["recording"]["title"])
+        print("\t" + track["recording"]["title"])
         discography[title_num].append(track["recording"]["title"])
-    print(discography[title_num])
+    #print(discography[title_num])
 
 discography = []
 
-"""
-for title_num in range(len(album_list)):
-    with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
-        executor.map(
-    #x = Thread(target=add_tracks, args=(title_num, discography, input_artist, album_list))
-    #x.start()
-    #add_tracks(title_num, discography, input_artist, album_list)
 
+for num in range(len(album_list)):
+    add_tracks(num, discography, input_artist, album_list)
 
-
-def thread_function(name):
-    time.sleep(2)
-    print(name)
-
-if __name__ == "__main__":
-    for i in range(5):
-        x = threading.Thread(target=thread_function, args=(i,))
-        x.start()
-
-arr = []
-
-arr.append([1])
-arr[0].append(2)
-arr[0].append(3)
-arr[0].append(4)
-arr[0].append(5)
-
-print(arr)
-"""
+print(discography)
