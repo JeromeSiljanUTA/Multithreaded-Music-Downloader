35605b480fa28cd596dec01c82f3de59ebebd56a
Jerome Siljan <Jerome.Siljan@mavs.uta.edu>
2022-04-17 00:34:23 -0500

    added multithreaded functionality + README.md

---
+++ README.md
@@ -0,0 +1,23 @@
+# Multithreaded Music Downloader
+
+This is a multithreaded music downloader. These are the steps it takes:
+ - User inputs an artists' name
+ - Search for all albums (excluding EPs, singles, mixes, etc.)
+ - Find all tracks in these albums
+ - Download the tracks by searching for them using `youtube-dl`
+ 
+ The downloading occurs in parallel thanks to `ThreadPoolExecuter`
+
+## Install
+Dependencies:
+```
+pip install python-musicbrainzngs
+youtube-dl
+```
+
+## Run 
+```
+python main.py
+```
+
+*I will add more documentation later*
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import threading
 import time
+import os
 import musicbrainzngs
+from concurrent.futures import ThreadPoolExecutor
 
 musicbrainzngs.set_useragent("Jerome's Music Scraper", "0.1", "jerome.siljan@mavs.uta.edu")
 
-#input_artist = input("Which artist are you looking for? ")
-input_artist = "Maluma"
+input_artist = input("Which artist are you looking for? ")
+#input_artist = "Maluma"
 
 artist_id = musicbrainzngs.search_artists(input_artist)["artist-list"][0]["id"]
 
@@ -13,7 +15,7 @@ artist_id = musicbrainzngs.search_artists(input_artist)["artist-list"][0]["id"]
 raw_releases = musicbrainzngs.get_artist_by_id(artist_id, 
         includes=["release-groups"], release_type=["album", "ep"])
 
-print("Albums:")
+#print("Albums:")
 
 album_list = []
 album_list_id = []
@@ -22,20 +24,45 @@ for release_group in raw_releases["artist"]["release-group-list"]:
     album_list.append(release_group["title"])
 
 def add_tracks(title_num, discography, input_artist, album_list):
-    print("Tracks in " + album_list[title_num] + ":")
+    #print("Tracks in " + album_list[title_num] + ":")
     discography.append([album_list[title_num]])
     album_id = (musicbrainzngs.search_releases(artist=input_artist, release=album_list[title_num], limit=1))["release-list"][0]["id"]
     raw_tracks_list = musicbrainzngs.get_release_by_id(album_id, includes=["recordings"])
     tracks_list = (raw_tracks_list["release"]["medium-list"][0]["track-list"])
     for track in tracks_list:
-        print("\t" + track["recording"]["title"])
+        #print("\t" + track["recording"]["title"])
         discography[title_num].append(track["recording"]["title"])
     #print(discography[title_num])
 
 discography = []
 
+def ytdl(args):
+    cmd = 'youtube-dl --extract-audio --audio-format mp3 --add-metadata "ytsearch:'
+    cmd += args + ' audio"'
+    os.system(cmd)
 
 for num in range(len(album_list)):
     add_tracks(num, discography, input_artist, album_list)
 
-print(discography)
+#add_tracks(0, discography, input_artist, album_list)
+
+executor = ThreadPoolExecutor(max_workers=4)
+for album in discography:
+    for song in album:
+        song_args = input_artist + ' ' + song
+        executor.submit(ytdl, args=song_args)
+
+
+
+"""
+def ytd(cmd):
+    os.system(cmd)
+
+for song in discography[0]:
+    cmd = 'youtube-dl --extract-audio --audio-format mp3 --add-metadata "ytsearch:'
+    args = input_artist + ' ' + song
+    cmd += args + ' audio"'
+    ytd(cmd)
+    a = threading.Thread(target=ytd, args=(cmd,))
+    a.start()
+    """
