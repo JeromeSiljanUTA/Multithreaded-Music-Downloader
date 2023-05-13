a804de8c41a426273910b100e644ff3e8ca21cee
Jerome Siljan <Jerome.Siljan@mavs.uta.edu>
2023-05-13 00:53:36 -0500

    feat(repo): update

--- main.py
+++ main.py
@@ -14,19 +14,22 @@ try:
     file_dest = args.file_path
     num_threads = args.integers
 except:
-    print('\nUsage: python main.py <destination> <number of threads>')
-    print('Recommended number of threads to use is 4')
+    print("\nUsage: python main.py <destination> <number of threads>")
+    print("Recommended number of threads to use is 4")
     exit()
 
-musicbrainzngs.set_useragent("Jerome's Music Scraper", "0.1", "jerome.siljan@mavs.uta.edu")
+musicbrainzngs.set_useragent(
+    "Jerome's Music Scraper", "0.1", "jerome.siljan@mavs.uta.edu"
+)
 
 input_artist = input("Which artist are you looking for? ")
 
 artist_id = musicbrainzngs.search_artists(input_artist)["artist-list"][0]["id"]
 
 
-raw_releases = musicbrainzngs.get_artist_by_id(artist_id, 
-        includes=["release-groups"], release_type=["album", "ep"])
+raw_releases = musicbrainzngs.get_artist_by_id(
+    artist_id, includes=["release-groups"], release_type=["album", "ep"]
+)
 
 album_list = []
 album_list_id = []
@@ -35,22 +38,31 @@ album_list_id = []
 for release_group in raw_releases["artist"]["release-group-list"]:
     album_list.append(release_group["title"])
 
+
 def add_tracks(title_num, discography, input_artist, album_list):
     discography.append([album_list[title_num]])
-    album_id = (musicbrainzngs.search_releases(artist=input_artist, release=album_list[title_num], limit=1))["release-list"][0]["id"]
-    raw_tracks_list = musicbrainzngs.get_release_by_id(album_id, includes=["recordings"])
-    tracks_list = (raw_tracks_list["release"]["medium-list"][0]["track-list"])
+    album_id = (
+        musicbrainzngs.search_releases(
+            artist=input_artist, release=album_list[title_num], limit=1
+        )
+    )["release-list"][0]["id"]
+    raw_tracks_list = musicbrainzngs.get_release_by_id(
+        album_id, includes=["recordings"]
+    )
+    tracks_list = raw_tracks_list["release"]["medium-list"][0]["track-list"]
     for track in tracks_list:
         discography[title_num].append(track["recording"]["title"])
 
+
 discography = []
 
+
 def ytdl(song_name, album_name):
-    args = input_artist + ' ' + song_name
+    args = input_artist + " " + song_name
     cmd = 'youtube-dl --extract-audio --audio-format flac --output "'
     cmd += str(file_dest)
-    cmd += '/' + album_name + '/'
-    cmd += '/' + song_name 
+    cmd += "/" + album_name + "/"
+    cmd += "/" + song_name
     cmd += '.%(ext)s" "ytsearch:'
     cmd += args + ' audio"'
     os.system(cmd)
@@ -59,28 +71,30 @@ def ytdl(song_name, album_name):
     cmd += album_name + '" --song "'
     cmd += song_name + '"'
     cmd += ' "' + str(file_dest)
-    cmd += '/' + album_name + '/'
+    cmd += "/" + album_name + "/"
     cmd += song_name + '.flac"'
-    #print('\n\n' + cmd + '\n\n')
+    # print('\n\n' + cmd + '\n\n')
     os.system(cmd)
 
 
-print('Type the numbers of the albums you want to download: ')
+print("Type the numbers of the albums you want to download: ")
 
 for num in range(len(album_list)):
     add_tracks(num, discography, input_artist, album_list)
-    print(str(num) + '. ' + album_list[num])
+    print(str(num) + ". " + album_list[num])
 
-print('-----------------------------------------------------')
-desired_albums = input('>> ')
+print("-----------------------------------------------------")
+desired_albums = input(">> ")
 arr_desired_albums = desired_albums.split()
 
 album_index_num = 0
 executor = ThreadPoolExecutor(max_workers=num_threads)
 for album in discography:
     if str(album_index_num) in arr_desired_albums:
-        os.mkdir(str(file_dest) + '/' + str(album_list[album_index_num]))
+        os.mkdir(str(file_dest) + "/" + str(album_list[album_index_num]))
         for song in album:
-            executor.submit(ytdl, song_name=song, album_name=album_list[album_index_num])
+            executor.submit(
+                ytdl, song_name=song, album_name=album_list[album_index_num]
+            )
 
     album_index_num += 1
